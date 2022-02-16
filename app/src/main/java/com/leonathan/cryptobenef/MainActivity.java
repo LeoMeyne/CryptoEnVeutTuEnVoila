1afea2dcbfe73f63f170d3086620e4e0a6cd46a8
Iwaz <nathandelorme.pro@gmail.com>
2022-02-16 10:26:15 +0100

    Project init

---
+++ app/src/main/java/com/leonathan/cryptobenef/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leonathan.cryptobenef;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
