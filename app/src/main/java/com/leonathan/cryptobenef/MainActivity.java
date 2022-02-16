71bd32abd49393f6319288019c872a16299609ef
Iwaz <nathandelorme.pro@gmail.com>
2022-02-16 11:05:21 +0100

    Project init

--- app/src/main/java/com/leonathan/cryptobenef/MainActivity.java
+++ app/src/main/java/com/leonathan/cryptobenef/MainActivity.java
@@ -1,7 +1,12 @@
 package com.leonathan.cryptobenef;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t = findViewById(R.id.cryptoText);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.coinbase.com/v2/prices/spot?currency=USD")
+                .build();
+        System.out.print(request.toString());
     }
 }
